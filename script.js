7d0d9bdcf2ed13ecc89cea7e858f6f41c017c176
Onegreat <wangreat@yandex.ru>
2022-12-08 22:16:01 +0300

    Выполнил работу

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-alert('Всем привет')
+function start(x) {
+    function askNum() {
+
+        let numAsk = +prompt("Введите число от 1 до 100")
+
+
+        if (numAsk === 0 || numAsk === undefined) {
+            alert("Игра окончена")
+            return;
+        }
+
+        if (isNumber(numAsk) == false) {
+            console.log("Вы ввели не число")
+            askNum()
+        }
+
+        if (numAsk > x) {
+            console.log("Вы ввели число больше загаданного.")
+            askNum()
+        } else if (numAsk < x) {
+            console.log("Вы ввели число меньше загаданного.")
+            askNum()
+        } else if (numAsk == x) {
+            alert("Вы выиграли!")
+        }
+    }
+    askNum()
+
+}
+
+const isNumber = function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+}
+
+start(10)
+
+
+
+// function isNumber()
